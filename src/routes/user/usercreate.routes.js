e8ee6663c1a3c17c702c91972a9d61f17ed6fabf
KevsonDoc <kevsonfilipesantos@gmail.com>
2020-07-26 23:11:50 -0300

    [add] - created route create user

--- src/routes/user/usercreate.routes.js
+++ src/routes/user/usercreate.routes.js
@@ -1,17 +1,23 @@
 const express = require("express");
 
 const knex = require("../../database");
+const { insert } = require("../../database");
 
 const routes = express.Router();
 
 routes.post("/user", async(request, response) => {
-    const { name, email, senha } = await request.body;
+    const { name, email } = await request.body;
+    const { senha } = await request.headers;
     try {
+        const emaildb = await knex("users").where("email", email).first();
+        if(emaildb) {
+            response.status(400).send({ message: "Este email esta em uso"})
+        }
         await knex("users").insert({ name, email, senha })
+        response.status(201).send({ message: "Conta criada com sucesso" })
     } catch (error) {
         console.log(error);
     }
-    response.send({ name, email, senha });
 });
 
 routes.delete("/delete_evething", async(request, response) => {
